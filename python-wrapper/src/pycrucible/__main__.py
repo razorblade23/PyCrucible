2d1dba06498f48d83079f8b6f39bba7555cfb595
razorblade23 <josip.vivo.cubric@gmail.com>
2025-06-28 18:42:08 +0200

    Updated pipeline to build artifacts for PyPi using maturin

--- python-wrapper/src/pycrucible/__main__.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import sys
-from pathlib import Path
-
-def main():
-    bin_path = Path(__file__).parent / "pycrucible-bin"
-    result = subprocess.run([str(bin_path)] + sys.argv[1:], check=False)
-    sys.exit(result.returncode)
