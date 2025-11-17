081b2946d7347332670ea673dedbc8fc60323d29
razorblade23 <josip.vivo.cubric@gmail.com>
2025-11-17 23:30:00 +0100

    Use only one `-q` instead of `-qq` Fixes: #40

--- runner/src/run.rs
+++ runner/src/run.rs
@@ -65,6 +65,7 @@ fn run_uv_command(
     ))?;
     let status = Command::new(&uv_path)
         .arg(command)
+        .arg("-q")
         .args(args)
         .current_dir(project_dir)
         .status()?;
@@ -104,10 +105,10 @@ pub fn run_extracted_project(project_dir: &Path, runtime_args: Vec<String>) -> i
     apply_env_from_config(&config);
 
     // Create virtual environment
-    let _venv = run_uv_command(project_dir, "venv", &["-qq"])?;
+    let _venv = run_uv_command(project_dir, "venv", &[])?;
 
-    // Sync virtual environment with the manifest file
-    let _pip_sync = run_uv_command(project_dir, "pip", &["install", "-qq", "--requirements", manifest_path.to_str().unwrap()])?;
+    // Sincronize the virtual environment with the manifest file
+    let _pip_sync = run_uv_command(project_dir, "pip", &["install", "--requirements", manifest_path.to_str().unwrap()])?;
     
     // Grab the hooks from config and unwrap them to a tuple
     let (pre_hook, post_hook) = prepare_hooks(&config);
